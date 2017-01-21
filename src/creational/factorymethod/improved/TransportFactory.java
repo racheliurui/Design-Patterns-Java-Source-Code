bfe995c95615a0a900cf58e85ca8f2dd3501dbb5
Rachel Rui Liu <liuruibnu@gmail.com>
2017-01-21 12:46:15 +0800

    backup
    java design patterns

---
+++ src/creational/factorymethod/improved/TransportFactory.java
@@ -0,0 +1,7 @@
+package creational.factorymethod.improved;
+//http://docs.oracle.com/javase/tutorial/java/IandI/abstract.html
+public abstract class TransportFactory {
+
+	abstract Transport create();
+	
+}
